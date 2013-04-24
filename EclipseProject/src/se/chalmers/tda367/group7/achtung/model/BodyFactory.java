429a68faf74d93965556e366150dac348b54bff1
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-04-24 10:47:37 +0200

    Changed so BodyFactory uses random Position method.

--- EclipseProject/src/se/chalmers/tda367/group7/achtung/model/BodyFactory.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/BodyFactory.java
@@ -1,27 +1,21 @@
 package se.chalmers.tda367.group7.achtung.model;
 
-import java.util.Random;
-
 public class BodyFactory {
 	
 	private BodyFactory(){};
 	
 	public static Body getBody(float worldWidth, float worldHeight) {
-		Random rand = new Random();
 		
 		int xLimiter = (int) (worldWidth * 0.1);
 		int yLimiter = (int) (worldHeight * 0.1);
 		
-		int xMax = (int) worldWidth - xLimiter;
-		int xMin = xLimiter;
-		int yMax = (int) (worldHeight - yLimiter);
-		int yMin = yLimiter;
-
-		float x = rand.nextInt(xMax - xMin + 1) + xMin;
-		float y = rand.nextInt(yMax - yMin + 1) + yMin;
+		int maxX = (int) worldWidth - xLimiter;
+		int minX = xLimiter;
+		int maxY = (int) (worldHeight - yLimiter);
+		int minY = yLimiter;
 		
 		float rot = (float) (Math.random() * 360);
-		Position position = new Position(x, y);
+		Position position = Position.getRandomPosition(minX, maxX, minY, maxY);
 		Body body = new Body(position, rot);
 		return body;
 	}
