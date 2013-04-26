50a16eba4c914ea5334afc8caf80991ff471c8eb
Joel Torstensson <oed3933@gmail.com>
2013-04-26 11:20:52 +0200

    Added missing methods in Position.

--- EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Position.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Position.java
@@ -35,4 +35,35 @@ public class Position {
 		float randY = (float) (minY + Math.random() * (maxY - minY));
 		return new Position(randX, randY);
 	}
+	
+	@Override
+	public String toString() {
+		return "x = " + x + "; y = " + y;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
+	
 }
