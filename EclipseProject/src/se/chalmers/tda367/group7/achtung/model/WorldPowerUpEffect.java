a9ba18899e329dcde384a3336fd1cd1d3e9e700b
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-05-06 11:11:47 +0200

    Changed parameter name in WorldPowerUpEffect.

--- EclipseProject/src/se/chalmers/tda367/group7/achtung/model/WorldPowerUpEffect.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/WorldPowerUpEffect.java
@@ -2,7 +2,7 @@ package se.chalmers.tda367.group7.achtung.model;
 
 public interface WorldPowerUpEffect extends PowerUpEffect {
 
-	void applyEffect(Round world);
+	void applyEffect(Round round);
 	
-	void removeEffect(Round world);
+	void removeEffect(Round round);
 }
