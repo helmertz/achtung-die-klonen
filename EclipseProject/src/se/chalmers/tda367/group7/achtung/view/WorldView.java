59c18a4731410cb09b65d0025592ff15ce70805c
Niklas Helmertz <niklas.helmertz@gmail.com>
2013-05-06 12:01:20 +0200

    Display who is the winner after round

--- EclipseProject/src/se/chalmers/tda367/group7/achtung/view/WorldView.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/view/WorldView.java
@@ -5,6 +5,7 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import se.chalmers.tda367.group7.achtung.model.Color;
 import se.chalmers.tda367.group7.achtung.model.Game;
 import se.chalmers.tda367.group7.achtung.model.Player;
 import se.chalmers.tda367.group7.achtung.model.PowerUpEntity;
@@ -17,6 +18,11 @@ public class WorldView implements View, PropertyChangeListener {
 	private final List<PowerUpEntityView> powerUpView = new ArrayList<PowerUpEntityView>();
 	private MapView mapView;
 	private final Game game;
+
+	private static final String NEXT_ROUND_MESSAGE_1 = "Round over";
+	private static final String NEXT_ROUND_MESSAGE_2 = "The winner is";
+	private static final String NEXT_ROUND_MESSAGE_3 = "Press space to continue";
+	private static final Color NEXT_ROUND_COLOR = new Color(0, 0, 0, 0.5f);
 
 	public WorldView(Game game) {
 		this.game = game;
@@ -38,7 +44,31 @@ public class WorldView implements View, PropertyChangeListener {
 		for (View view : this.powerUpView) {
 			view.render(renderer, interpolation);
 		}
-
+		// If over draws a box displaying the winner, and instructions on how to
+		// continue.
+		if (!game.getCurrentRound().isRoundActive()) {
+		
+			// TODO perhaps not hardcode as much here. Maybe redo size handling
+			// in font.
+			float viewWidth = renderer.getViewAreaWidth();
+			float viewHeight = renderer.getViewAreaHeight();
+			float centerX = viewWidth / 2;
+			float centerY = viewHeight / 2;
+			float width = 600;
+			float height = 240;
+			Player winner = game.getCurrentRound().getWinner();
+			String name = winner.getName();
+			renderer.drawFilledRect(centerX - width / 2, centerY - height / 2,
+					width, height, NEXT_ROUND_COLOR);
+			renderer.drawStringCentered(NEXT_ROUND_MESSAGE_1, centerX,
+					centerY - 55, 2.5f);
+			renderer.drawStringCentered(NEXT_ROUND_MESSAGE_2, centerX,
+					centerY - 15, 1.5f);
+			renderer.drawStringCentered(name, centerX, centerY + 60, 3,
+					winner.getColor());
+			renderer.drawStringCentered(NEXT_ROUND_MESSAGE_3, centerX,
+					centerY + 105, 1f);
+		}
 	}
 
 	private void updatePowerUpViews() {
