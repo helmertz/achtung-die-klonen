d51966f1482e68f196da6c1ecc8bc322041daa20
Jakob Csorgei Gustavsson <soulrivers@gmail.com>
2013-05-08 11:45:50 +0200

    Forgot new BodyPowerUpEffect class.

---
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/BodyPowerUpEffect.java
@@ -0,0 +1,31 @@
+package se.chalmers.tda367.group7.achtung.model;
+
+import se.chalmers.tda367.group7.achtung.model.PowerUpEntity.Type;
+
+public interface BodyPowerUpEffect extends PowerUpEffect {
+
+	/**
+	 * Add the effect of the power up to a body.
+	 * 
+	 * @param body
+	 *            - the body that gets the power up added
+	 */
+	void applyEffect(Body body);
+
+	/**
+	 * Remove the effect of the power up from a body.
+	 * 
+	 * @param player
+	 *            - the body that gets the power up removed
+	 */
+	void removeEffect(Body body);
+
+	/**
+	 * Returns true if the type is allowed for the particular power up
+	 * 
+	 * @param type
+	 *            - who the power up affects
+	 * @return true - if type is allowed
+	 */
+	boolean isTypeOk(Type type);
+}
