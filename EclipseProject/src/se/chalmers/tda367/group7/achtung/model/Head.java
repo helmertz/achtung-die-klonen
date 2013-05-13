3fc1cc0fee2be72e6d00a4ab6b5a03d11784591a
LucasPi <lucaspersson93@gmail.com>
2013-05-13 14:54:27 +0200

    Hashcode and equals here and there

--- EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Head.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Head.java
@@ -25,4 +25,40 @@ public class Head {
 	public void setPosition(Position position) {
 		this.position = position;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(diameter);
+		result = prime * result
+				+ ((position == null) ? 0 : position.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Head other = (Head) obj;
+		if (Float.floatToIntBits(diameter) != Float
+				.floatToIntBits(other.diameter)) {
+			return false;
+		}
+		if (position == null) {
+			if (other.position != null) {
+				return false;
+			}
+		} else if (!position.equals(other.position)) {
+			return false;
+		}
+		return true;
+	}
 }
--- EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Map.java
+++ EclipseProject/src/se/chalmers/tda367/group7/achtung/model/Map.java
@@ -44,4 +44,43 @@ public class Map {
 		return this.wallsActive;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((backgroundColor == null) ? 0 : backgroundColor.hashCode());
+		result = prime * result + Float.floatToIntBits(height);
+		result = prime * result + Float.floatToIntBits(width);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Map other = (Map) obj;
+		if (backgroundColor == null) {
+			if (other.backgroundColor != null) {
+				return false;
+			}
+		} else if (!backgroundColor.equals(other.backgroundColor)) {
+			return false;
+		}
+		if (Float.floatToIntBits(height) != Float.floatToIntBits(other.height)) {
+			return false;
+		}
+		if (Float.floatToIntBits(width) != Float.floatToIntBits(other.width)) {
+			return false;
+		}
+		return true;
+	}
+
 }
