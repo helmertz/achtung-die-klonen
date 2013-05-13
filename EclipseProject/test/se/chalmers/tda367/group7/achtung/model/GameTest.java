a8a4db1245adb1082bce637b3ab40178829f9dfd
Joel Torstensson <oed3933@gmail.com>
2013-05-13 10:52:35 +0200

    GameTest created.

---
+++ EclipseProject/test/se/chalmers/tda367/group7/achtung/model/GameTest.java
@@ -0,0 +1,56 @@
+package se.chalmers.tda367.group7.achtung.model;
+
+import static org.junit.Assert.*;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	private Game game;
+	
+	@Before
+	public void createGame() {
+		game = new Game();
+	}
+	
+	@Test
+	public void testGetNewPlayer() {
+		Player p1 = game.getNewPlayer("player1", Color.getRandomColor());
+		assertTrue(p1.getName().equals("player1"));
+		
+		assertTrue(game.getPlayers().size() == 1);
+	}
+	
+	@Test
+	public void testNewRound() {
+		game.addPropertyChangeListener(new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				assertTrue(evt.getPropertyName().equals("NewRound"));
+				
+			}
+			
+		});
+		game.newRound();
+	}
+	
+	@Test
+	public void testIsOver() {
+		game.getNewPlayer("player1", Color.getRandomColor());
+		game.getNewPlayer("player2", Color.getRandomColor());
+		game.newRound();
+		for (int i = 0; i < 3000; i++) {
+			game.update();
+			// only creates round if none active.
+			game.newRound();
+		}
+		
+		assertTrue(game.isOver());
+	}
+
+}
